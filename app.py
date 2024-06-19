f43998e080796db594ca767cdac517ca5c16ffe9
ntirushwajeanmarc <159898931+ntirushwajeanmarc@users.noreply.github.com>
2024-06-19 07:40:54 +0000

    i added new files

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
   return render_template('index.html')
 
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
-  

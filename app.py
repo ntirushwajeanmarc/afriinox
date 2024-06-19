d4f898927697c4e686e0dc92b758129e74ec98ef
ntirushwajeanmarc <159898931+ntirushwajeanmarc@users.noreply.github.com>
2024-06-19 10:02:21 +0000

    i added new pages

--- app.py
+++ app.py
@@ -13,5 +13,12 @@ def about():
   return render_template('about.html')
 
 
+@app.route('/services')
+def services():
+  return render_template('ourservices.html')
+@app.route('/projects')
+def projects():
+  return render_template('projects.html')
+
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
